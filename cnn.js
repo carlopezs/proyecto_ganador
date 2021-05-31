1c3ba7fb66a87166f5edf05cfa14158493997152
Carlos Lopez <carlos@nousproyect.com>
2021-05-31 17:36:04 -0500

    upload app

--- cnn.js
+++ cnn.js
@@ -1,7 +1,4 @@
-const pgPromise = require('pg-promise')
-
-
-
+const pgPromise = require("pg-promise");
 /* const config = {
     host: 'localhost',
     port: '5432',
@@ -10,12 +7,21 @@ const pgPromise = require('pg-promise')
     password: ''
 }
  */
- const config = {
-    connectionString: 'postgres://fuagkvarykkvnv:19991ee6e0785925a0e0a92e4c6f41413f040b98127659738d0f5684cec09706@ec2-18-214-195-34.compute-1.amazonaws.com:5432/da0keclgv0oob3',
-    
+const config = {
+  host: 'ec2-18-214-195-34.compute-1.amazonaws.com',
+  port: '5432',
+  database: 'da0keclgv0oob3',
+  user: 'fuagkvarykkvnv',
+  password: '19991ee6e0785925a0e0a92e4c6f41413f040b98127659738d0f5684cec09706'
 }
 
-const pgp = pgPromise({})
-pgp.pg.defaults.ssl = false;
-const db = pgp(config)
-exports.db = db 
+/* const config = {
+  connectionString:
+    "postgres://fuagkvarykkvnv:19991ee6e0785925a0e0a92e4c6f41413f040b98127659738d0f5684cec09706@ec2-18-214-195-34.compute-1.amazonaws.com:5432/da0keclgv0oob3",
+}; */
+
+
+
+const pgp = pgPromise({});
+const db = pgp(config);
+exports.db = db;
