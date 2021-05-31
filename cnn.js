8abc596ae632102e5487e8dcbed9118f9f4ca9e4
Carlos Lopez <carlos@nousproyect.com>
2021-05-30 20:50:52 -0500

    make it better

--- cnn.js
+++ cnn.js
@@ -12,9 +12,9 @@ const config = {
     port: '5432',
     database: 'da0keclgv0oob3',
     user: 'fuagkvarykkvnv',
-    password: ''
+    password: '19991ee6e0785925a0e0a92e4c6f41413f040b98127659738d0f5684cec09706'
 }
 const pgp = pgPromise({})
 const db = pgp(config)
-
+db.any();
 exports.db = db
