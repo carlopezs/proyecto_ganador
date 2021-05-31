17ef324fdd853abca04053292b50216a597c219e
Carlos Lopez <carlos@nousproyect.com>
2021-05-30 19:33:25 -0500

    make it better

--- cnn.js
+++ cnn.js
@@ -1,10 +1,10 @@
 const pgPromise = require('pg-promise')
 const config = {
-    host: 'localhost',
+    host: 'ec2-18-214-195-34.compute-1.amazonaws.com',
     port: '5432',
-    database: 'stock',
-    user: 'postgres',
-    password: ''
+    database: 'da0keclgv0oob3',
+    user: 'fuagkvarykkvnv',
+    password: '19991ee6e0785925a0e0a92e4c6f41413f040b98127659738d0f5684cec09706'
 }
 const pgp = pgPromise({})
 const db = pgp(config)
