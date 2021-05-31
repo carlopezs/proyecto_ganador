ad8b7c8ba1b1256676f338a6a7aa00fa3be9a218
Carlos Lopez <carlos@nousproyect.com>
2021-05-30 21:26:32 -0500

    make it better

--- cnn.js
+++ cnn.js
@@ -1,4 +1,7 @@
 const pgPromise = require('pg-promise')
+
+
+
 /* const config = {
     host: 'localhost',
     port: '5432',
@@ -7,14 +10,13 @@ const pgPromise = require('pg-promise')
     password: ''
 }
  */
-const config = {
-    host: 'ec2-18-214-195-34.compute-1.amazonaws.com',
-    port: '5432',
-    database: 'da0keclgv0oob3',
-    user: 'fuagkvarykkvnv',
-    password: '19991ee6e0785925a0e0a92e4c6f41413f040b98127659738d0f5684cec09706'
+ const config = {
+    connectionString: 'postgres://fuagkvarykkvnv:19991ee6e0785925a0e0a92e4c6f41413f040b98127659738d0f5684cec09706@ec2-18-214-195-34.compute-1.amazonaws.com:5432/da0keclgv0oob3',
+    
 }
+
 const pgp = pgPromise({})
+pgp.pg.defaults.ssl = true;
 const db = pgp(config)
 db.any();
-exports.db = db
+exports.db = db 
