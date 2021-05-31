dd061cd9d8d2be9b4dbd97dc02c2dbb3d6613f83
Carlos Lopez <carlos@nousproyect.com>
2021-05-31 18:17:00 -0500

    change database

--- cnn.js
+++ cnn.js
@@ -1,18 +1,19 @@
 const pgPromise = require("pg-promise");
+
 /* const config = {
     host: 'localhost',
     port: '5432',
-    database: 'supermarket',
+    database: 'stock',
     user: 'postgres',
     password: ''
-}
- */
+} */
+
 const config = {
-  host: 'ec2-18-214-195-34.compute-1.amazonaws.com',
+  host: 'database2.cjdcw0u0axpy.us-east-2.rds.amazonaws.com',
   port: '5432',
-  database: 'da0keclgv0oob3',
-  user: 'fuagkvarykkvnv',
-  password: '19991ee6e0785925a0e0a92e4c6f41413f040b98127659738d0f5684cec09706'
+  database: 'supermercado',
+  user: 'postgres',
+  password: '12345678'
 }
 
 /* const config = {
