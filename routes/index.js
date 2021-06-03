6cc78907f8a81b427930e7e8022b93c4cff51b7d
Erick Sevilla <epsevillae@utn.edu.ec>
2021-06-03 09:03:51 -0500

    CRUD productos (revisar el de SelectById)

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const {Router} = require('express')
-const { getProducts } = require('../controllers/inventario.controllers')
+const { getProducts, getProductsById, postCreateProduct, putUpdateProduct, deleteDeleteProducto} = require('../controllers/inventario.controllers')
 const route = Router()
 
 route.get('/',(req,res) => {
     res.send('Welcome API-REST Inventario!!!!')})
 
 route.get('/productos',getProducts)
+route.get('/productos/:id', getProductsById)
+route.post('/productos', postCreateProduct)
+route.put('/productos', putUpdateProduct)
+route.delete('/productos', deleteDeleteProducto)
 
 
 module.exports = route
