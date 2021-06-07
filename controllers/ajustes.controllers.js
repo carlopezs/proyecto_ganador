db47dc5ef4ee513db1ba090aee371f9483898b51
JohnG6667 <60703340+JohnG6667@users.noreply.github.com>
2021-06-07 16:14:43 -0500

    CreateAjusteDetalle

--- controllers/ajustes.controllers.js
+++ controllers/ajustes.controllers.js
@@ -71,16 +71,16 @@ const postCreateAjusteCabecera = async (req, res) => {
 
 
 const postCreateAjusteDetalle = async (req, res) => {
-    const { det_cantidad, det_suma, cab_id, pro_id} = req.query;
+    const { det_cantidad, cab_id, pro_id} = req.query;
     const sql = `INSERT INTO public.ajustes_detalle(
-        det_cantidad, det_suma, cab_id, pro_id)
-        VALUES ($1, $2, $3, $4);`;
+      det_cantidad, cab_id, pro_id, det_stock_registro)
+      VALUES ($1, $2, $3, (select pro_stock from product where pro_id = $4));`;
     try {
-      const response = await db.any(sql, [det_cantidad, det_suma, cab_id, pro_id]);
+      const response = await db.any(sql, [det_cantidad, cab_id, pro_id, pro_id]);
       res.json({
         message: "Detalle creado con exito",
         body: {
-          detalle: { det_cantidad, det_suma, cab_id, pro_id },
+          detalle: { det_cantidad, cab_id, pro_id },
         },
       });
     } catch (error) {
